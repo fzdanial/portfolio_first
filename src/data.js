56b50c32f05184334576f879a37114477cee7e5c
Danial Zulkifli <fzdanial09@gmail.com>
2023-06-02 05:37:08 +0800

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "React Dashboard",
+    subtitle: "MERN Stack",
+    description:
+      "Empty",
+    image: "./project-1.gif",
+    link: "https://github.com/fzdanial",
+  },
+  {
+    title: "Ruby Project Management",
+    subtitle: "Ruby on Rails",
+    description:
+    "Empty",
+    image: "./project-2.gif",
+    link: "https://github.com/fzdanial",
+  },
+  {
+    title: "Desktop Cleaning App",
+    subtitle: "Python",
+    description:
+    "Empty",
+    image: "./project-3.gif",
+    link: "https://github.com/fzdanial",
+  },
+  {
+    title: "Machine Learning Work",
+    subtitle: "Tensorflow",
+    description:
+    "Empty",
+    image: "./project-4.gif",
+    link: "https://github.com/fzdanial",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "The quick brown fox jumped over the lazy dog",
+    image: "",
+    name: "Client One",
+    company: "Company1",
+  },
+  {
+    quote:
+      "The quick brown fox jumped over the lazy dog",
+    image: "",
+    name: "Client Two",
+    company: "Company2",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "Python",
+  "TensorFlow",
+  "SciKitLearn",
+  "Ruby",
+  "Rails",
+];
